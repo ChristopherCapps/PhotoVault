06d505e4eb8d1430ae1e730d5908d24b23f6054d
Christopher Capps <capps@christoshibambp.lan>
2019-11-14 17:05:51 -0500

    adding media query module'

--- Exiftool.fs
+++ Exiftool.fs
@@ -5,33 +5,29 @@ open FSharp.Data
 open FSharp.Data.JsonExtensions
 
 [<Literal>]
+let EnvExiftoolPath = "EXIFTOOL_PATH"
+[<Literal>]
 let DefaultExiftoolPath = "exiftool"
 
 [<Literal>]
 let private JsonOutputFlag = "-json"
 
-type ExiftoolPath = 
-    | DefaultToolPath
-    | AbsoluteToolPath of string
+let private ExiftoolPath = 
+    Environment.environVarOrDefault EnvExiftoolPath DefaultExiftoolPath
 
 type ExiftoolArgs = string seq
 
 type private ExiftoolResult = 
-    | Failure of exitCode: int
+    | Failure of exitCode: int * errorMessage: string
     | Success of output: string * error: string
 
 type TagCollection =
     | AllTags
     | NamedTags of string seq
 
-let private runExiftool exiftoolPath (args:ExiftoolArgs) =
-    let exiftoolPath = 
-        match exiftoolPath with
-        | DefaultToolPath -> DefaultExiftoolPath
-        | AbsoluteToolPath path -> path
-
+let private runExiftool (args:ExiftoolArgs) =
     let result = 
-        CreateProcess.fromRawCommand exiftoolPath args
+        CreateProcess.fromRawCommand ExiftoolPath args
         |> CreateProcess.redirectOutput
         |> Proc.run
 
@@ -39,15 +35,24 @@ let private runExiftool exiftoolPath (args:ExiftoolArgs) =
     | 0 -> 
         Success(result.Result.Output, result.Result.Error)
     | error -> 
-        Failure(error)
+        Failure(error, result.Result.Error)
+
+type MediaQueryResult =
+    | Failure of exitCode: int * errorMessage: string
+    | Success of JsonValue
+
+let private mediaQuery args mediaPath = 
+    seq { yield mediaPath; yield JsonOutputFlag; yield! args }
+    |> runExiftool 
 
 let getMediaTags tags mediaPath =
     let tagArguments =
         match tags with
-        | AllTags -> System.String.Empty
-        | NamedTags tags -> tags |> Seq.fold (sprintf "%s -%s") System.String.Empty
+        | AllTags -> []
+        | NamedTags tags -> Seq.toList tags
 
-    let result = runExiftool DefaultToolPath [mediaPath; tagArguments; JsonOutputFlag]
+    let result = runExiftool (List.append [mediaPath; JsonOutputFlag] tagArguments)
 
     match result with 
-    | Failure exitCode -> 
+    | ExiftoolResult.Failure (exitCode, errorMessage) -> Failure (exitCode, errorMessage)
+    | ExiftoolResult.Success (out, err) ->  Success (JsonValue.Parse out)
--- Program.fs
+++ Program.fs
@@ -6,18 +6,16 @@ open Fake.Core.CreateProcess
 open FSharp.Data
 open FSharp.Data.JsonExtensions
 
-//let (?) (s:string) propertyName = s + propertyName
-    //let x = "Test" ? test ? help
-    //printfn "Result: %A" x
+open Exiftool
 
 [<EntryPoint>]
 let main argv =
-    let results = 
-        fromRawCommand "exiftool" ["/Users/capps/Downloads/Photos"; "-json"]
-        |> redirectOutput
-        |> Proc.run
-
-    let output = JsonValue.Parse results.Result.Output
-    for photo in output do        
-        printfn "File: %A DTO: %A" photo?FileName photo?FileModifyDate
-    0 // return an integer exit code
+    let results = getMediaTags AllTags "/Users/capps/Downloads/Photos"
+    match results with
+    | Failure (exitCode, errorMessage) ->
+        printfn "Error: %d; %s" exitCode errorMessage
+        exitCode
+    | Success json -> 
+        for photo in json do        
+            printfn "File: %A DTO: %A" photo?FileName photo?FileModifyDate
+        0
