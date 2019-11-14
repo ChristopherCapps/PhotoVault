6e8b81da9cf40aa45726c391d238deee2d378e24
ChristopherCapps <chris.capps@live.com>
2019-11-14 06:42:50 -0500

    updating Exiftool

--- Exiftool.fs
+++ Exiftool.fs
@@ -1,14 +1,53 @@
 module Exiftool
 
 open Fake.Core
-open Fake.Core.CreateProcess
 open FSharp.Data
 open FSharp.Data.JsonExtensions
 
+[<Literal>]
+let DefaultExiftoolPath = "exiftool"
 
-let results = 
-    fromRawCommand "exiftool" ["/Users/capps/Downloads/Photos"; "-json"]
-    |> redirectOutput
-    |> Proc.run
+[<Literal>]
+let private JsonOutputFlag = "-json"
 
-let x= results.
+type ExiftoolPath = 
+    | DefaultToolPath
+    | AbsoluteToolPath of string
+
+type ExiftoolArgs = string seq
+
+type private ExiftoolResult = 
+    | Failure of exitCode: int
+    | Success of output: string * error: string
+
+type TagCollection =
+    | AllTags
+    | NamedTags of string seq
+
+let private runExiftool exiftoolPath (args:ExiftoolArgs) =
+    let exiftoolPath = 
+        match exiftoolPath with
+        | DefaultToolPath -> DefaultExiftoolPath
+        | AbsoluteToolPath path -> path
+
+    let result = 
+        CreateProcess.fromRawCommand exiftoolPath args
+        |> CreateProcess.redirectOutput
+        |> Proc.run
+
+    match result.ExitCode with
+    | 0 -> 
+        Success(result.Result.Output, result.Result.Error)
+    | error -> 
+        Failure(error)
+
+let getMediaTags tags mediaPath =
+    let tagArguments =
+        match tags with
+        | AllTags -> System.String.Empty
+        | NamedTags tags -> tags |> Seq.fold (sprintf "%s -%s") System.String.Empty
+
+    let result = runExiftool DefaultToolPath [mediaPath; tagArguments; JsonOutputFlag]
+
+    match result with 
+    | Failure exitCode -> 
